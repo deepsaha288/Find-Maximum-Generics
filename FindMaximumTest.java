72e959329ce2cbaca4a317aae9de3a2778874cb4
Deep Saha <deepsaha288@yahoo.com>
2021-03-11 00:44:33 +0530

    given number greater should return number using String

--- FindMaximumTest.java
+++ FindMaximumTest.java
@@ -3,33 +3,24 @@ import org.junit.jupiter.api.Test;
 
 class FindMaximumTest {
 
- @Test
-    public void given_num1_should_return_num1() {
+   @Test
+    public void given_num1_greater_should_return_num() {
         FindMaximum findMax = new FindMaximum();
-        Float number1 = (float) 8.5;
-        Float number2 = (float) 5.6;
-        Float number3 = (float) 6.8;
-        Float maxNo = findMax.giveMaximum(number1,number2,number3);
-        Assertions.assertEquals(number1,maxNo);
+        String maxNo = findMax.giveMaximum("Orange","Apple","Mango");
+        Assertions.assertEquals("Orange",maxNo);
     }
 
     @Test
-    public void given_num2_should_return_num2() {
+    public void given_num2_greater_should_return_num() {
         FindMaximum findMax = new FindMaximum();
-        Float number1 = (float) 6.8;
-        Float number2 = (float) 8.5;
-        Float number3 = (float) 5.6;
-        Float maxNo = findMax.giveMaximum(number1,number2,number3);
-        Assertions.assertEquals(number2,maxNo);
+        String maxNo = findMax.giveMaximum("Apple","Orange","Mango");
+        Assertions.assertEquals("Orange",maxNo);
     }
 
     @Test
-    public void given_num3_should_return_num3() {
+    public void given_num3_greater_should_return_num() {
         FindMaximum findMax = new FindMaximum();
-        Float number1 = (float) 5.6;
-        Float number2 = (float) 6.8;
-        Float number3 = (float) 8.5;
-        Float maxNo = findMax.giveMaximum(number1,number2,number3);
-        Assertions.assertEquals(number3,maxNo);
+        String maxNo = findMax.giveMaximum("Mango","Apple","Orange");
+        Assertions.assertEquals("Orange",maxNo);
     }
 }
